8d16fe41b2b6d6dce6c13815df220c4212adff13
Simon Mueller <simon.mueller@a-sit.at>
2024-08-08 11:13:19 +0200

    unify initWithVCK spelling

--- mobiledrivinglicence/src/commonMain/kotlin/at/asitplus/wallet/mdl/Initializer.kt
+++ mobiledrivinglicence/src/commonMain/kotlin/at/asitplus/wallet/mdl/Initializer.kt
@@ -17,14 +17,14 @@ object Initializer {
      * A reference to this class is enough to trigger the init block
      */
     init {
-        initWithVck()
+        initWithVCK()
     }
 
     /**
      * This has to be called first, before anything first, to load the
      * relevant classes of this library into the base implementations of VC-K
      */
-    fun initWithVck() {
+    fun initWithVCK() {
         LibraryInitializer.registerExtensionLibrary(
             credentialScheme = MobileDrivingLicenceScheme,
             serializerLookup = serializerLookup(),
