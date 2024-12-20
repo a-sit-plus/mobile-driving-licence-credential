d6b558f4c47d47f9944e48c598bcaa3d7191e895
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-12-20 12:44:29 +0100

    prepare regression testing for VC-K

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,25 @@ pluginManagement {
     }
 }
 
+if (System.getProperty("regressionTest") == "true") {
+    includeBuild("../signum") {
+        dependencySubstitution {
+            substitute(module("at.asitplus.wallet:indispensable")).using(project(":indispensable"))
+            substitute(module("at.asitplus.signum:indispensable-josef")).using(project(":indispensable-josef"))
+            substitute(module("at.asitplus.signum:indispensable-cosef")).using(project(":indispensable-cosef"))
+            substitute(module("at.asitplus.signum:supreme")).using(project(":supreme"))
+        }
+    }
+    includeBuild("..") {
+        dependencySubstitution {
+            substitute(module("at.asitplus.wallet:vck")).using(project(":vck"))
+            substitute(module("at.asitplus.wallet:vck-openid")).using(project(":vck-openid"))
+            substitute(module("at.asitplus.wallet:vck-openid-ktor")).using(project(":vck-openid-ktor"))
+            substitute(module("at.asitplus.wallet:openid-data-classes")).using(project(":openid-data-classes"))
+        }
+    }
+}
+
+
 rootProject.name = "mobile-driving-licence"
 include(":mobiledrivinglicence")
