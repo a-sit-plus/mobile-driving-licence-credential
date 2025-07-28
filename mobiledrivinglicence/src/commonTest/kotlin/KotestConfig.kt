2ac4d7c8cd65449d2c30a3135a2456aaf6409acb
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-07-28 15:10:35 +0200

    fix test wiring

--- mobiledrivinglicence/src/commonTest/kotlin/KotestConfig.kt
+++ mobiledrivinglicence/src/commonTest/kotlin/KotestConfig.kt
@@ -1,8 +1,13 @@
 package io.kotest.provided
 import at.asitplus.test.XmlReportingProjectConfig
 import at.asitplus.test.JUnitXmlReporter
+import at.asitplus.wallet.mdl.Initializer
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.extensions.Extension
 
 /** Wires KMP JUnit XML reporting */
-class ProjectConfig : XmlReportingProjectConfig()
+class ProjectConfig : XmlReportingProjectConfig() {
+    init {
+        Initializer.initWithVCK()
+    }
+}
